a6b42fcc67e7f63dd0eeb21d92d47072d067a1fd
Isratul Aini <134768596+isrtlaini@users.noreply.github.com>
2024-05-21 17:20:24 +0700

    Add files via upload

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+ 
+class AuthController extends Controller
+{
+ 
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+ 
+    public function register()
+    {
+        return view('auth/register');
+    }
+ 
+    public function registerSave(Request $request)
+    {
+        Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed'
+        ])->validate();
+ 
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'type' => "0"
+        ]);
+ 
+        return redirect()->route('login');
+    }
+ 
+    public function login()
+    {
+        return view('auth/login');
+    }
+ 
+    public function loginAction(Request $request)
+    {
+        Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ])->validate();
+ 
+        
+    }
+}
